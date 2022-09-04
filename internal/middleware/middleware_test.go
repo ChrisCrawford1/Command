c75188440933d588285fa5f201c541b8b6423efa
Christopher Crawford <christopher.crawford1@outlook.com>
2022-09-04 19:34:40 +0100

    Add Validate Jwt Token handler type test

--- internal/middleware/middleware_test.go
+++ internal/middleware/middleware_test.go
@@ -7,13 +7,29 @@ import (
 )
 
 func TestContentTypeMiddleware(t *testing.T) {
-	var testHandler TestHandler
-	mH := ContentTypeMiddleware(&testHandler)
+	t.Run("Will run with expected handler provided", func(t *testing.T) {
+		var testHandler TestHandler
+		mH := ContentTypeMiddleware(&testHandler)
 
-	switch v := mH.(type) {
-	case http.Handler:
-		// Do nothing as this is the type we expect
-	default:
-		t.Error(fmt.Sprintf("%T received unexpected type for ContentTypeMiddleware", v))
-	}
+		switch v := mH.(type) {
+		case http.Handler:
+			// Do nothing as this is the type we expect
+		default:
+			t.Error(fmt.Sprintf("%T received unexpected type for ContentTypeMiddleware", v))
+		}
+	})
+}
+
+func TestValidateJwtToken(t *testing.T) {
+	t.Run("Will run with expected handler provided", func(t *testing.T) {
+		var testHandler TestHandler
+		mH := ValidateJwtToken(&testHandler)
+
+		switch v := mH.(type) {
+		case http.Handler:
+			// Do nothing as this is the type we expect
+		default:
+			t.Error(fmt.Sprintf("%T received unexpected type for ValidateJwtToken", v))
+		}
+	})
 }
